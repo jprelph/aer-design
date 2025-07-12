469483e8b220b7156a84f7c76a1bd559f6edb932
jprelph <james.relph@capgemini.com>
2025-07-12 19:58:59 +0100

    Fully working Terraform

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -7,7 +7,7 @@ data "aws_availability_zones" "available_primary" {
 }
 
 data "aws_availability_zones" "available_secondary" {
-  provider = aws.primary
+  provider = aws.secondary
   filter {
     name   = "opt-in-status"
     values = ["opt-in-not-required"]
@@ -110,19 +110,26 @@ module "eks_secondary" {
 resource "aws_db_subnet_group" "events_primary" {
   provider   = aws.primary
   name       = "events"
-  subnet_ids = slice(data.aws_availability_zones.available_primary.names, 0, 3)
+  subnet_ids = module.vpc.private_subnets
 }
 
 resource "aws_rds_cluster" "primary" {
   provider                  = aws.primary
   engine                    = aws_rds_global_cluster.events.engine
   engine_version            = aws_rds_global_cluster.events.engine_version
+  engine_mode               = "provisioned"
   cluster_identifier        = "events-primary-cluster"
   master_username           = "username"
   master_password           = "somepass123"
   database_name             = "events_db"
   global_cluster_identifier = aws_rds_global_cluster.events.id
   db_subnet_group_name      = aws_db_subnet_group.events_primary.name
+  skip_final_snapshot       = true
+  serverlessv2_scaling_configuration {
+    max_capacity             = 1.0
+    min_capacity             = 0.0
+    seconds_until_auto_pause = 3600
+  }
 }
 
 resource "aws_rds_cluster_instance" "primary" {
@@ -140,18 +147,24 @@ resource "aws_rds_cluster_instance" "primary" {
 resource "aws_db_subnet_group" "events_secondary" {
   provider   = aws.secondary
   name       = "events"
-  subnet_ids = slice(data.aws_availability_zones.available_secondary.names, 0, 3)
+  subnet_ids = module.vpc_secondary.private_subnets
 }
 
 resource "aws_rds_cluster" "secondary" {
   provider                  = aws.secondary
   engine                    = aws_rds_global_cluster.events.engine
   engine_version            = aws_rds_global_cluster.events.engine_version
+  engine_mode               = "provisioned"
   cluster_identifier        = "events-secondary-cluster"
   global_cluster_identifier = aws_rds_global_cluster.events.id
   db_subnet_group_name      = aws_db_subnet_group.events_secondary.name
+  skip_final_snapshot       = true
   enable_global_write_forwarding = true
-
+  serverlessv2_scaling_configuration {
+    max_capacity             = 1.0
+    min_capacity             = 0.0
+    seconds_until_auto_pause = 3600
+  }
   lifecycle {
     ignore_changes = [
       replication_source_identifier
