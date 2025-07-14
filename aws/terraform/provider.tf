1a0bc6d1d08af0888dfb8e029caaa8ebe0c50f2a
jprelph <james.relph@capgemini.com>
2025-07-14 10:21:52 +0100

    Add S3 state storage

--- aws/terraform/provider.tf
+++ aws/terraform/provider.tf
@@ -16,3 +16,12 @@ provider "aws" {
   alias   = "secondary"
   region  = var.sec_region
 }
+
+terraform {
+  backend "s3" {
+    bucket = "aer-tf"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
